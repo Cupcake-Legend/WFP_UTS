9edb684c4c4e0ff9c74dc0ca021b64c0f0f3e350
Cupcake-Legend <sutantodarren@gmail.com>
2025-04-22 11:34:03 +0700

    Add resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\MenuController;
+use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\RewardController;
+use App\Http\Controllers\UserController;
+use App\Models\OrderDetail;
+use App\Models\PaymentMethod;
+use App\Models\RewardDetail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +25,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource("users", UserController::class);
+
+Route::resource("menus", MenuController::class);
+
+Route::resource("paymentMethods", PaymentMethod::class);
+
+Route::resource("categories", CategoryController::class);
+
+Route::resource("notifications", NotificationController::class);
+
+Route::resource("rewards", RewardController::class);
+
+Route::resource("rewardDetails", RewardDetail::class);
+
+Route::resource("orders", OrderController::class);
+
+Route::resource("orderDetails", OrderDetail::class);
