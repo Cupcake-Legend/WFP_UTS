0bd84e944ac2763516082cbd740defe316d0f48f
Cupcake-Legend <sutantodarren@gmail.com>
2025-04-22 11:34:57 +0700

    Fix naming error

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,10 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderDetailController;
+use App\Http\Controllers\PaymentMethodController;
 use App\Http\Controllers\RewardController;
+use App\Http\Controllers\RewardDetailController;
 use App\Http\Controllers\UserController;
 use App\Models\OrderDetail;
 use App\Models\PaymentMethod;
@@ -30,7 +33,7 @@ Route::resource("users", UserController::class);
 
 Route::resource("menus", MenuController::class);
 
-Route::resource("paymentMethods", PaymentMethod::class);
+Route::resource("paymentMethods", PaymentMethodController::class);
 
 Route::resource("categories", CategoryController::class);
 
@@ -38,8 +41,8 @@ Route::resource("notifications", NotificationController::class);
 
 Route::resource("rewards", RewardController::class);
 
-Route::resource("rewardDetails", RewardDetail::class);
+Route::resource("rewardDetails", RewardDetailController::class);
 
 Route::resource("orders", OrderController::class);
 
-Route::resource("orderDetails", OrderDetail::class);
+Route::resource("orderDetails", OrderDetailController::class);
